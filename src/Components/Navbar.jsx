3fcc4d2f65aff01d9a29262616f17bcc0ba0f637
aru-shi2 <arushimishra2929@gmail.com>
2025-08-22 19:27:26 -0600

    added navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,11 +3,15 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <nav>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
+        <nav className='bg-purple-700 p-3 font-bold text-white'>
+            <ul className='flex justify-between'>
+                <div className="logo">
+                    <li className='ml-6 text-2xl'>Taskly</li>
+                </div>
+                <div className='flex gap-4 mr-5 text-xl'>
+                    <li>Home</li>
+                    <li>Your Tasks</li>
+                </div>
             </ul>
         </nav>
     </div>
