39026278227ff66bf4ebaddc490f7dcdab054800
aru-shi2 <arushimishra2929@gmail.com>
2025-08-23 13:05:25 -0600

    added input

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './Components/Navbar'
 
 function App() {
@@ -8,10 +6,17 @@ function App() {
 
   return (
     <>
-    <Navbar/>
-      <div className="container">
-        
+      <Navbar/>
+      <div className="bg1 h-screen overflow-hidden">
+        <div className="container space-y-10 mx-auto w-[60%]">
+          <h1 className='flex justify-center items-center text-5xl pt-9 text-white font-bold'>Todo List</h1>
+          <div className="write flex gap-3 bg-white">
+            <input className='bg-gray-700 w-[85%] h-8 ml-9' type="text" placeholder='Add a new todo..'/>
+          <button className='bg-red-500 px-4 rounded'>Add</button>
+          </div>
+        </div>
       </div>
+      
     </>
   )
 }
