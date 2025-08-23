bd6686c894e72aab80ca8e4816cd1c4811eb9258
aru-shi2 <arushimishra2929@gmail.com>
2025-08-23 14:57:58 -0600

    basic ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,47 @@
-import { useState } from 'react'
-import Navbar from './Components/Navbar'
+import { useState } from "react";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Navbar/>
       <div className="bg1 h-screen overflow-hidden">
-        <div className="container space-y-10 mx-auto w-[60%]">
-          <h1 className='flex justify-center items-center text-5xl pt-9 text-white font-bold'>Todo List</h1>
-          <div className="write flex gap-3 bg-white">
-            <input className='bg-gray-700 w-[85%] h-8 ml-9' type="text" placeholder='Add a new todo..'/>
-          <button className='bg-red-500 px-4 rounded'>Add</button>
+        <Navbar />
+        <div className="container space-y-5 mx-auto w-[60%]  h-screen">
+          <h1 className="flex justify-center items-center text-5xl pt-9 font-bold">
+            Todo List
+          </h1>
+          <div className="write flex gap-3">
+            <input
+              className="bg-gray-700 w-[85%] h-8 ml-7 rounded placeholder-gray-400 p-4"
+              type="text"
+              placeholder="Add a new todo.."
+            />
+            <button className="bg-red-500 px-4 rounded">Add</button>
           </div>
+          <div className="show ml-9 border-b-1 flex gap-3">
+            <input type="checkbox" id="show" />
+            <label htmlFor="show">Show Finished</label>
+          </div>
+          <div className="todos ml-9 flex flex-col gap-2">
+            <h1 className="font-bold text-xl">Your todos</h1>
+            <div className="todo flex gap-4">
+              <div className="text">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Temporibus, illum!
+              </div>
+              <div className="btns space-x-3 mb-4">
+                <button className="bg-purple-500 px-4 rounded">Edit</button>
+                <button className="bg-purple-500 px-4 rounded h-6">Delete</button>
+              </div>
+            </div>
+          </div>
+          
         </div>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav className='bg-purple-700 p-3 font-bold text-white'>
             <ul className='flex justify-between'>
                 <div className="logo">
-                    <li className='ml-6 text-2xl'>Taskly</li>
+                    <li className='ml-9 text-2xl'>Taskly</li>
                 </div>
                 <div className='flex gap-4 mr-5 text-xl'>
                     <li>Home</li>
