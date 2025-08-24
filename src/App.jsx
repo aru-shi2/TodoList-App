bcdfdeb21e48be8eb0e7477db0e617d9c4ce7662
aru-shi2 <arushimishra2929@gmail.com>
2025-08-23 20:25:19 -0600

    added fonts

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,34 @@ function App() {
 
   return (
     <>
-      <div className="bg1 h-screen overflow-hidden">
+      <div className="bg1 h-screen overflow-hidden pacifico-regular">
         <Navbar />
-        <div className="container space-y-5 mx-auto w-[60%]  h-screen">
-          <h1 className="flex justify-center items-center text-5xl pt-9 font-bold">
+        <div className="container space-y-6 mx-auto w-[67%]  h-screen ">
+          <h1 className="flex justify-center items-center text-5xl pt-9 font-bold text-white">
             Todo List
           </h1>
           <div className="write flex gap-3">
             <input
-              className="bg-gray-700 w-[85%] h-8 ml-7 rounded placeholder-gray-400 p-4"
+              className="bg-gray-700 w-[85%] h-8 ml-11 rounded-xl placeholder-gray-400 p-5"
               type="text"
               placeholder="Add a new todo.."
             />
-            <button className="bg-red-500 px-4 rounded">Add</button>
+            <button className="bg-purple-900 px-4 rounded-xl text-white">Add</button>
           </div>
-          <div className="show ml-9 border-b-1 flex gap-3">
+          <div className="show ml-10 pb-2 border-b-1 flex gap-3">
             <input type="checkbox" id="show" />
             <label htmlFor="show">Show Finished</label>
           </div>
-          <div className="todos ml-9 flex flex-col gap-2">
-            <h1 className="font-bold text-xl">Your todos</h1>
+          <div className="todos ml-9 flex flex-col gap-3">
+            <h1 className="font-bold text-2xl">Your todos</h1>
             <div className="todo flex gap-4">
-              <div className="text">
+              <div className="text-xl">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Temporibus, illum!
               </div>
               <div className="btns space-x-3 mb-4">
-                <button className="bg-purple-500 px-4 rounded">Edit</button>
-                <button className="bg-purple-500 px-4 rounded h-6">Delete</button>
+                <button className="bg-purple-900 px-4 rounded h-7 text-gray-300">Edit</button>
+                <button className="bg-purple-900 px-3 rounded h-7 text-gray-300">Delete</button>
               </div>
             </div>
           </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <nav className='bg-purple-700 p-3 font-bold text-white'>
+        <nav className='bg-purple-900 p-3 font-bold text-white'>
             <ul className='flex justify-between'>
                 <div className="logo">
                     <li className='ml-9 text-2xl'>Taskly</li>
                 </div>
-                <div className='flex gap-4 mr-5 text-xl'>
+                <div className='flex gap-8 mr-5 text-xl'>
                     <li>Home</li>
                     <li>Your Tasks</li>
                 </div>
