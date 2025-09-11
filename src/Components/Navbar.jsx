5ee1cb1e225e74b9248044fd0c68fb146c06bee7
aru-shi2 <arushimishra2929@gmail.com>
2025-09-10 22:08:24 -0600

    nav ui improved

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav className='bgnav p-3 font-bold text-white'>
             <ul className='flex justify-between'>
                 <div className="logo">
-                    <li className='ml-9 text-2xl'>Taskly</li>
+                    <li className='ml-9 md:text-2xl text-xl'>Taskly</li>
                 </div>
                 <div className='flex gap-8 mr-5 text-xl '>
                     <li className='transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer'>Home</li>
